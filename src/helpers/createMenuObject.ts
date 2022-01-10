c7757143b289e8c6c5c6df8e17c6d235b69134d6
ThatyGama <thatyanegamac@gmail.com>
2022-01-10 13:02:32 -0300

    concluded project

---
+++ src/helpers/createMenuObject.ts
@@ -0,0 +1,14 @@
+type MenuOptions = '' | 'all' | 'dog' | 'cat' | 'fish';
+
+export const createMenuObject = (activeMenu: MenuOptions) => {
+    let returnObject = {
+        all: false,
+        dog: false,
+        cat: false,
+        fish: false
+    };
+
+    (activeMenu !== '')? returnObject[activeMenu] = true:'';
+
+    return returnObject;
+};
